a48bf403ec6c0e421f587da3a4aa3fec0a71dc93
Sasitorn <sasitorn@MacBook-Pro-khxng-Sasitorn.local>
2026-06-23 15:55:09 +0700

    ใส่ข้อความอธิบายงานของ branch Micky

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["10.114.247.189"],
+  allowedDevOrigins: [
+    "10.114.240.216",
+    "10.114.247.189",
+    "10.114.*.*",
+    "10.*.*.*",
+    "192.168.*.*",
+  ],
   reactStrictMode: true,
 };
 
