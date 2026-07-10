54f15cf2b58dbb216ae6c57053388092399ae35a
Sasitorn <sasitorn@MacBook-Pro-khxng-Sasitorn.local>
2026-07-10 13:45:25 +0700

    Fix combobox selection in dialogs

--- components/ui/combobox-input.tsx
+++ components/ui/combobox-input.tsx
@@ -62,7 +62,7 @@ export function ComboboxInput({
   }, [open]);
 
   return (
-    <Popover open={open} onOpenChange={handleOpenChange}>
+    <Popover open={open} onOpenChange={handleOpenChange} modal>
       <PopoverTrigger asChild>
         <Button
           type="button"
--- components/ui/combobox-select.tsx
+++ components/ui/combobox-select.tsx
@@ -59,7 +59,7 @@ export function ComboboxSelect({
   }, [open]);
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
+    <Popover open={open} onOpenChange={setOpen} modal>
       <PopoverTrigger asChild>
         <Button
           type="button"
